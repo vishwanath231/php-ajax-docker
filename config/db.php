302cd31d04b1fde49335c51e65ffaa2c254edbaa
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-10-05 12:16:24 +0530

    database configuration

---
+++ config/db.php
@@ -0,0 +1,27 @@
+<?php
+
+$host = 'db';
+
+// Database use name
+$user = 'vishwanath';
+
+//database user password
+$pass = 'vishwanath';
+
+$db = 'user';
+
+$conn = new mysqli($host,$user,$pass,$db);
+
+
+
+// if ($conn->connect_error) {
+//    echo "Not connected, error: " . $conn->connect_error;
+// }
+// else
+// {
+//    echo "Connected.";
+// }
+
+
+
+?>
